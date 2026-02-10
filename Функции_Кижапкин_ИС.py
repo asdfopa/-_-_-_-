98871f1f9470f7ff6017467c2c98faa551fd4289
asdfopa <132782776+asdfopa@users.noreply.github.com>
2026-02-10 13:04:47 +0300

    Add files via upload

--- Функции_Кижапкин_ИС.py
+++ Функции_Кижапкин_ИС.py
@@ -1,50 +1,48 @@
-#board_games = ['Settlers of Catan', 'Carcassone', 'Power Grid', 'Agricola', 'Scrabble']
-#sport_games = ['football', 'football - American', 'hockey', 'baseball', 'cricket']
-#for sport in sport_games:
-#    print(sport) задание 1
+def create_spread_sheet(title:str, row_count=1000):
+    print(f'Создание электронной таблицы с названием {title} with {row_count} lines')
+create_spread_sheet('Приложения', 10)
 
 
 
 
-#promise = 'I will not chew gum in class'
-#for i in range(5):
-#    print(promise) задание 2
 
 
 
 
-#students_period_A = ["Alex", "Briana", "Cheri", "Daniele"]
-#students_period_B = ["Dora", "Minerva", "Alexa", "Obie"]
-#for name in students_period_A:
-#    students_period_B.append(students_period_A)
-#print(students_period_B) задание 3
 
 
 
 
-#dog_breeds_available_for_adoption =['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
-#dog_breed_I_want = 'dalmatian'
-#for dog in dog_breeds_available_for_adoption:
-#    if dog == dog_breed_I_want:
-#        break
-#print("У них есть собака, которую я хочу!") задание 4
 
 
-#sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
-#scoops_sold = 0
-#for team_of_sales_data in sales_data:
-#    for data in team_of_sales_data:
-#        scoops_sold += int(data)
-#print(scoops_sold) задание 5
 
 
-single_digits = [0,1,2,3,4,5,6,7,8,9]
-squares = [namber**2 for namber in single_digits]
-for namber in single_digits:
-    print(namber)
-print(squares)
-cubes = [namber**3 for namber in single_digits]
-print(cubes) #Задание 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

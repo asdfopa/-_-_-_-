26485654d8a9e30682f8da29c137b9650bd9078e
asdfopa <132782776+asdfopa@users.noreply.github.com>
2026-02-10 09:52:50 +0300

    Add files via upload

---
+++ Управляемые потоки.py
@@ -0,0 +1,92 @@
+#if (6*6)-1>=8+1:
+#    print('Все верно')
+#else:
+#    print('Неверно')
+
+#if 13-7<=(3*2)+1:
+#    print('Все верно')
+#else:
+#    print('Неверно')
+
+#if 3*(2-1)>4-1:
+ #   print('Все верно')
+#else:
+ #   print('Неверно') задание 1
+
+
+#bool_variable = 'true'
+#print(bool_variable)
+#print(type(bool_variable))
+#bool_variable_2 = True
+#print(type(bool_variable_2)) задание 2
+
+
+user_name = input('Введите имя')
+Dmitriy_check = 'Дмитрий, твое рабочее место находится в другой комнате. Отойди от чужого компьютера и займись работой'
+enter_number = 0
+dobro_pozhalovat = 'Добро пожаловать'
+if user_name == ' Дмитрий':
+   print(Dmitriy_check)
+   enter_number+=1
+if user_name == ' Ангелина':
+    print(dobro_pozhalovat)
+    enter_number+=1
+    if enter_number <= 3:
+        print('Попробуйте еще раз. У вас осталось ' + str(3 - enter_number) + ' попыток')
+
+
+
+#if (2+2+2 >= 6) and (-1* -1 <0):
+#    print('Верно')
+#else:
+ #   print('Неверно')
+
+#if (4*2 <= 8) and (7-1 == 6):
+#    print('Верно')
+#else:
+#    print('Неверно') Задание 3
+
+
+
+#if (2-1 > 3) or (-5*2 == -10):
+#    print('Верно')
+#else:
+#    print('Неверно')
+
+#if (9+5 <= 15) or (7 != 4+3):
+#    print('Верно')
+#else:
+#    print('Неверно') задание 4
+
+
+#grade = float(input('Введите средний балл '))
+
+#if grade >= 4.0:
+#    print('A')
+#elif grade < 4.0 and grade >= 3.0:
+#    print('B')
+#elif grade < 3.0 and grade >= 2.0:
+#    print('C')
+#elif grade < 2.0 and grade >= 1.0:
+#    print('D')
+#elif grade < 1.0 and grade > 0.0:
+#    print('F') задание 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

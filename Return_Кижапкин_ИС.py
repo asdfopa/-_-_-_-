a2ca8874158c82378a73213cefdf7108ba40d06b
asdfopa <132782776+asdfopa@users.noreply.github.com>
2026-02-10 14:09:12 +0300

    Add files via upload

---
+++ Return_Кижапкин_ИС.py
@@ -0,0 +1,76 @@
+"""def cale_age(current_year, birth_year):
+    return current_year - birth_year
+
+my_age = cale_age(2049, 1993)
+dads_age = cale_age(2049, 1953)
+print(f'Мне {my_age} лет, а моему отцу {dads_age} лет')"""
+
+
+
+"""def get_boundaries(target: int, margin: int):
+    low_limit = target - margin
+    high_limit = margin + target
+    return low_limit, high_limit
+low_limit, high_limit = get_boundaries(100,20)
+print(f'Нижний предел: {low_limit}, верхний предел: {high_limit}')
+"""
+
+
+
+
+"""def repeat_stuff(stuff, num_repeats=10):
+    return stuff*num_repeats
+
+lyrics = repeat_stuff('Row', 3) + 'Your Boat'
+song = repeat_stuff('Row')
+print(song)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

ccd43e89b24f80a04554b8cc967e7aea8c87ebd4
asdfopa <132782776+asdfopa@users.noreply.github.com>
2026-02-10 10:11:48 +0300

    Add files via upload

---
+++ Работа со строками_Кижапкин_ИС.py
@@ -0,0 +1,90 @@
+"""favour_word = "Cаня Череп"
+print(favour_word)""" #Задание 1
+
+
+"""first_name = "Виталий"
+last_name = "Красилов"
+new_account = last_name[:5]
+temp_password = last_name[3:7]
+print(new_account, temp_password)""" #Задание 2
+
+"""first_name = "Виталий"
+last_name = "Красилов"
+def account_generator(f_name, l_name):
+    return f_name[:3] + l_name[:3]
+new_account = account_generator(first_name, last_name)
+print(new_account)""" #Задание 3
+
+
+
+"""first_name = "Виталий"
+last_name = "Красилов"
+def password_generator(first_name, last_name):
+    return first_name[-3:] + last_name[-3:]
+temp_password = password_generator(first_name, last_name)
+print(temp_password)""" #Задание 4
+
+
+
+"""company_motto = "Мечты сбываются"
+second_to_last = company_motto[-2]
+final_word = company_motto[-4:]""" #Задание 5
+
+
+
+"""first_name = "Боб"
+fixed_first_name = "Р" + first_name[1:]
+print(fixed_first_name)""" #Задание 6
+
+
+#password = "theycallme\"crazy\"91" Задание 7
+
+
+
+"""poem_title = "spring storm"
+poem_title_fixed = poem_title.title()
+print(poem_title_fixed)
+print(poem_title)""" #Задание 8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

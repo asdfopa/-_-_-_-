93759d2976ec7f2ee070874812d9c2618f8c6b93
asdfopa <132782776+asdfopa@users.noreply.github.com>
2026-02-11 13:00:11 +0300

    Add files via upload

---
+++ Cписки1_Кижапкин_ИС.py
@@ -0,0 +1,110 @@
+#spisok = ['торт', 1] Задание 1
+
+
+#Household_chemicals = [['стиральный порошок', 1], ['средство для мытья посуды', 1]] Задание 2
+
+
+
+"""names = ['Ben', 'Holly', 'Ann']
+dogs_names = ['Sharik', 'Gab', 'Beethoven']
+
+list_of_names_and_dogs_names = list(zip(names, dogs_names))
+print(list_of_names_and_dogs_names)"""
+
+
+
+
+"""orders = ['маргаритки', 'васильки']
+orders.append('тюльпаны')
+orders.append('розы')
+print(orders)"""
+
+
+
+"""order = ['маргаритки', 'лютик', 'львиный зев', 'гардения', 'лилия']
+
+new_orders = order + ['сирень', 'ирис']
+broken_prices = [5,3,4,5,4] + [4]
+print(new_orders, broken_prices)"""
+
+
+
+
+"""list1 = range(9)
+list2 = range(8)
+print(list(list1), list(list2))"""
+
+
+"""list1 = list(range(5, 16, 3))
+print(list1)
+list2 = list(range(0, 41, 5))
+print(list2)"""
+
+
+
+"""first_names = ["Эйнсли", "Бен", "Чани", "Депак"]
+age = []
+age.append(42)
+all_ages = [32, 41, 29]
+name_and_age = list(zip(first_names, all_ages + age))
+ids = range(3)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Cписки2_Кижапкин_ИС.py
@@ -0,0 +1,118 @@
+"""list1 = list(range(2, 20, 3))
+list1_len = len(list1)
+print(list1_len)"""
+
+
+"""shopping_list = ["яйца", "масло", "молоко", "огурцы", "сок", "хлопья"]
+print(len(shopping_list))
+last_element = shopping_list[-1]
+element5 = shopping_list[5]
+print(last_element, element5)"""
+
+
+
+"""suitcase = ["рубашка", "рубашка", "брюки", "брюки", "пижамы", "книги"]
+beginning = suitcase[0:2]
+beginning = suitcase[:3]
+middle = suitcase[len(suitcase)//2-1:len(suitcase)//2 + 1]
+print(middle)"""
+
+
+
+
+"""suitcase = ["рубашка", "футболка", "носки", "очки", "пижама", "книги"]
+start = suitcase[:2]"""
+
+
+
+
+"""votes = ['Jake', 'Jake', 'Laurie', 'Laurie', 'Laurie', 'Jake', 'Jake', 'Jake', 'Laurie', 'Cassie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', 'Cassie', 'Jake',
+'Jake', 'Cassie', 'Laurie']
+jake_votes = votes.count("Jake")
+print(jake_votes)"""
+
+
+"""
+addresses = ['221 B Baker St.', '42 Wallaby Way', '12 Grimmauld Place', '742 Evergreen Terrace'
+, '1600 Pennsylvania Ave', '10 Downing St.']
+addresses.sort()
+print(addresses)"""
+
+
+
+"""games = ['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
+games_sorted = sorted(games)
+print(games_sorted)"""
+
+
+
+inventory = ["двухспальная кровать", "двухспальная кровать", "изголовье", "двуспальная кровать",
+"двуспальная кровать", "комод", "комод", "стол", "стол", "тумбочка", "тумбочка", "королевский кровать", "двуспальная кровать", "две односпальные кровати", "две односпальные кровати",
+"простыни", "простыни", "подушка", "подушка"]
+print(len(inventory))
+first = inventory[0]
+last = inventory[-1]
+inventory_2_6 = inventory[2:6]
+first_3 = inventory[2]
+twin_beds = inventory.count("две односпальные кровати") * 2
+inventory.sort()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

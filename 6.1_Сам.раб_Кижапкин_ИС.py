2bd1d02cbca40fff88ae4295c34eb0de17013d57
asdfopa <132782776+asdfopa@users.noreply.github.com>
2026-02-12 11:49:21 +0300

    Add files via upload

---
+++ 6.1_Сам.раб_Кижапкин_ИС.py
@@ -0,0 +1,124 @@
+"""def f_to_c(f_temp):
+    c_temp = (f_temp - 32) * 5/9
+    return c_temp
+f100_to_celsius = f_to_c(100)
+
+def c_to_f(c_temp):
+    f_temp = c_temp *9/5 + 32
+    return f_temp
+c0_in_fahrenheit = c_to_f(0)"""
+
+
+
+"""def get_force(m, a):
+    return m * a
+def get_energy(m, c = 3*10**8):
+    return m * c ** 2
+def get_work(m, a, distance):
+    f = get_force(m, a)
+    return f * distance
+bomb_mass = 1
+train_mass = 22600
+train_acceleration = 10
+train_distance = 100
+train_force = get_force(train_mass, train_acceleration)
+print(train_force)
+print(f"Поезд GE поставляет {train_force} Ньютонов силы")
+bomb_energy = get_energy(bomb_mass)
+print(f"1 кг бомбы составляет {bomb_energy} Джоулей")
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print(f"Поезд выполняет {train_work} Джоулей за {train_distance} метров")
+"""
+
+
+"""clothes = "домашняя одежда"
+def vivod(par1, par2):
+    print(f"{par1} лучше всего подходит {par2}")
+print("У меня большой гардероб")
+times_of_day = ["Утром", "Днём", "Вечером", "Ночью"]
+for i in times_of_day:
+    vivod(i, clothes)
+meal = ["каша", "суп", "курица"]
+times_to_eat = ["На завтрак", "На обед", "На ужин"]
+for i in range(len(times_to_eat)):
+    vivod(times_to_eat[i], meal[i])"""
+
+
+
+
+"""def f(grade: float):
+    if grade >= 4:
+        return "A"
+    elif grade >= 3:
+        return "B"
+    elif grade >= 2:
+        return "C"
+    elif grade >= 1:
+        return "D"
+    elif grade >= 0:
+        return "F"
+grade = float(input())
+print(f(grade))"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Cловари лаба1_Кижапкин_ИС.py
@@ -0,0 +1,110 @@
+"""sensors = {"Living room": 21, "kitchen": 23, "bedroom": 20, "кладовая": 22}
+num_cameras = {"Backyard": 6, "garage": 2, "driveway": 1}
+print(num_cameras)"""
+
+
+
+"""slovar = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch"}
+print(slovar)"""
+
+
+
+"""animals_in_zoo = {}
+animals_in_zoo["Зебры"]=8
+animals_in_zoo["Обезьяны"]=12
+animals_in_zoo["Динозавры"]=0
+print(animals_in_zoo)"""
+
+
+
+"""user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "sringQueen": 85739})
+print(user_ids)"""
+
+
+
+"""oskar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Afflect", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oskar_winners["Best Picture"] = "Moonlight"
+print(oskar_winners)"""
+
+
+
+"""drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+zipped_drinks = list(zip(drinks, caffeine))
+drinks_to_caffeine = {key:value for key, value in zip(drinks, caffeine)}
+print(drinks_to_caffeine)"""
+
+
+"""songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+plays = {key:value for key, value in zip(songs, playcounts)}
+plays["Purple Haze"] = 1
+plays["Respect"] = 94
+pystoy_slovar = {}
+library = {"The Best Songs": plays, "Sunday feelings": pystoy_slovar}
+print(library)
+print(plays)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

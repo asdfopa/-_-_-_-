f53113aae2a4e2faf19c26ebfaf1a04eaeeac7bc
asdfopa <132782776+asdfopa@users.noreply.github.com>
2026-02-12 12:54:27 +0300

    Add files via upload

---
+++ Cловари_лаб2_Кижапкин_ИС.py
@@ -0,0 +1,113 @@
+"""zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+print(zodiac_elements["earth"])
+zodiac_elements["energy"] = "Not a zodiac element"
+key_to_check = "energy"
+if key_to_check in zodiac_elements:
+    print(zodiac_elements["energy"])"""
+
+
+"""caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120}
+caffeine_level["matcha"] = 30
+try:
+    print(caffeine_level["matcha"])
+except KeyError:
+    print('Неизвестный уровень кофеина')"""
+
+
+
+"""user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+tc_id = user_ids.get('teraCoder', 100000)
+print(tc_id)
+stack_id = user_ids.get('superStackSmash', 100000)
+print(stack_id)"""
+
+
+"""user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+users = user_ids.keys()
+classes = num_exercises.keys()
+print(users)
+print(classes)"""
+
+
+
+"""num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+total_exercises = num_exercises.values()
+print(total_exercises)"""
+
+
+
+"""tarot = { 1: "The Magician", 2: "The High Priestess", 3: "The Empress", 4: "The Emperor",5: "The Hierophant", 6: "The Lovers", 7: "The Chariot", 8: "Strength", 9: "The Hermit", 10:"Wheel of Fortune", 11: "Justice", 12: "The Hanged Man", 13: "Death", 14: "Temperance",15: "The Devil", 16: "The Tower", 17: "The Star", 18: "The Moon", 19: "The Sun", 20: "Judgement", 21: "The World", 22: "The Fool"}
+spread = {}
+key_past = tarot.get(13)
+key_present = tarot.get(22)
+key_future = tarot.get(10)
+spread["key past"] = key_past
+spread["key present"] = key_present
+spread["key future"] = key_future
+print(spread)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
